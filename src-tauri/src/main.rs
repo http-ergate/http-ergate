e08b0649114658a7d3e5a2d60361dc433135563e
Weidaicheng <wdcdavyc@outlook.com>
2022-11-12 13:48:09 +0800

    move greet command to example mod

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,11 +3,9 @@
     windows_subsystem = "windows"
 )]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+mod command;
+
+use command::example::greet;
 
 fn main() {
     tauri::Builder::default()
