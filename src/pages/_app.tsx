47a3dbf789388f4221cf0624d29bad83853942ee
Weidaicheng <wdcdavyc@outlook.com>
2022-11-13 15:04:14 +0800

    import and use nextui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
+import { NextUIProvider } from "@nextui-org/react";
 import type { AppProps } from "next/app";
-
-import "../style.css";
-import "../App.css";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
