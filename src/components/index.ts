94c140c8a7a4776b8fd37009334382860988748d
Weidaicheng <wdcdavyc@outlook.com>
2022-11-30 19:49:51 +0800

    add sense compon

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from "./food";
 export * from "./carry-method";
+export * from "./sense";
---
+++ src/components/sense.tsx
@@ -0,0 +1,70 @@
+import { Button, Grid, Input } from "@nextui-org/react";
+import { useState } from "react";
+import { DeleteIcon } from "../icons";
+
+/**
+ * sense props
+ */
+interface SenseProps {
+    /**
+     * sense changed
+     */
+    senseChanged: (key: string, value: string) => void,
+
+    /**
+     * sense deleted
+     */
+    senseDeleted: () => void,
+}
+
+/**
+ * sense of food
+ * @param props props
+ * @returns 
+ */
+export function Sense(props: SenseProps) {
+    const [key, setKey] = useState("");
+    const [value, setValue] = useState("");
+
+    props.senseChanged(key, value);
+
+    return (
+        <>
+            <Grid.Container
+                gap={0.5}
+                justify="flex-start"
+                alignItems="center"
+            >
+                {/* sense key */}
+                <Grid xs>
+                    <Input
+                        bordered
+                        fullWidth
+                        color="secondary"
+                        animated={false}
+                        onChange={(ev) => setKey(ev.currentTarget.value)}
+                    />
+                </Grid>
+                {/* sense value */}
+                <Grid xs>
+                    <Input
+                        bordered
+                        fullWidth
+                        color="secondary"
+                        animated={false}
+                        onChange={(ev) => setValue(ev.currentTarget.value)}
+                    />
+                </Grid>
+                {/* delete sense */}
+                <Grid>
+                    <Button
+                        auto
+                        color="error"
+                        icon={<DeleteIcon size={24} fill="#FFFFFF" />}
+                        onPress={() => {props.senseDeleted()}}
+                    />
+                </Grid>
+            </Grid.Container>
+        </>
+    );
+}
